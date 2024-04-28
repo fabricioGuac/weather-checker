fde4a264c53e85ba9798d9bc0584843cf4c95949
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-04-28 10:19:10 -0700

    delves on the logic behind the history btns, adds comments, adds a document.ready function, and delves on a function fusing latLon and weather getter

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,10 @@
-const history = $('#history');
+let history =  JSON.parse(localStorage.getItem("history")) || [];
+const today = $('#today');
+const forecast = $('#forecast');
+const historylist = $('#history');
 // Creates a variable to store the api key
 const Apikey = "64c061c7a143e44714946379a0b46c02";
+
 // Function to get the lat and lon from the city name 
 const  latLonGetter = async (city) => {
     try {
@@ -33,20 +37,62 @@ const weatherGetter = async (lat,lon) => {
     }catch(error){
         console.log(error);
     }
-}
+};
 
+// Function to use latLon and weather getter together
+const latLonWeather = (city) => {
+    latLonGetter(city)
+        .then(({ lat, lon }) => {
+            weatherGetter(lat, lon)
+                .then((weather) => {
+                    console.log(weather);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+
+// Function to create and render the history buttons on the page
 const renderHistory = (city) => {
     const  historyelcont = $('<li>');
     const  historyel = $('<button>');
-    historyel.text(city)
-    historyel.attr('class', 'btn btn-outline-info')
-    historyelcont.append(historyel)
-    history.append(historyelcont);
+    historyel.text(city);
+    historyel.attr('class', 'btn btn-outline-info');
+    historyelcont.append(historyel);
+    historylist.append(historyelcont);
+    localStorage.setItem('history', JSON.stringify(history));
+};
+
+// Function to handle the new searches 
+const weatherSearcher = (event) => {
+    event.preventDefault();
+    const city = $('#city').val().trim();
+    $('#city').val('');
+    if(!history.includes(city)){
+        history.push(city);
+        renderHistory(city);
+    };
+    latLonWeather(city);
 }
 
-// latLonGetter('davenport').then(({lat, lon}) => {
-//     weatherGetter(lat,lon).then((weather) => {
-//         console.log(weather);
-//     });
-// })
+// Function to get the weather from the history buttons
+const historybtn = (event) => {
+    event.preventDefault();
+    latLonWeather(event.target.textContent);
+}
 
+// When the page is fully loaded adds event listeners and loops over the history to get the history buttuns on the page
+$(document).ready(function () {
+    $('#search').on('click', weatherSearcher);
+
+    history.forEach(search => {
+        renderHistory(search);
+    });
+
+    historylist.on('click', historybtn)
+})
