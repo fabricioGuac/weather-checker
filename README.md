2aa75b350dcc343bbe5d7e6196145a37104944f7
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-04-30 23:20:29 -0700

    replaces the api for the lat and lon with an api that returns the current weather, the lat and lon and delves in the README.md file

---
+++ README.md
@@ -0,0 +1,50 @@
+# Weather-checker
+
+## Description 
+
+This project aims to develop a weather forecast page to improve planning activities outdoors based on the weather conditions, leveraging openWeather's geocoding and 5 day forecast APIs.
+
+## Instalation
+
+N/A
+
+## Usage
+
+Once you open it it will look for the weather conditions in the last new city you looked for, if there are none it will default to New York. In order to use this web application enter the name of the city you want to check the weather for in the search bar, click on the search button to initiaze the search.
+After searching for a city for a city, a button with the name of the city will appear in the History section bellow the search bar, click on any of the buttons to retrieve the weather information for that city again. 
+
+## Credits
+
+N/A
+
+## Screenshot displaying the deployed site
+
+![]()
+
+## URL to the deployed site
+
+https://fabricioguac.github.io/weather-checker/
+
+## License
+
+MIT License
+
+Copyright (c) 2024 fabricioGuac
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,11 @@
 // Gets the history of searches from local storage if there are no searches initializes an empty array
 let history =  JSON.parse(localStorage.getItem("history")) || [];
+// Selects the HTML elements for easier manipulation later in the code
 const today = $('#today');
 const forecast = $('#forecast');
 const historylist = $('#history');
 const h1 =$('h1');
+// Creates a boolean to check if there are error messages being displayed
 let h1error = false;
 // Creates a variable to store the api key
 const Apikey = "64c061c7a143e44714946379a0b46c02";
@@ -27,37 +29,36 @@ const errorMsg = (error) => {
 };
 
 
-
-// Function to get the lat and lon from the city name 
-const  latLonGetter = async (city) => {
+// Function to get the current weather
+const currentWeatherGetter = async (city) => {
     try {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${Apikey}`);
-    if(!response.ok){
-        throw new Error('Failed to fetch coodinates'); 
-    }
-    const data = await response.json();
-    const lat = data[0].lat;
-    const lon = data[0].lon;
-
-    console.log(lat, lon);
-    return{lat,lon};
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}&units=imperial`);
+        if(!response.ok){
+            throw new Error('Failed to fetch  current weather condition');
+        }
+        const data = await response.json();
+        console.log(data);
+        const crrntweather = data;
+        const lat = data.coord.lat;
+        const lon = data.coord.lon;
+        return {crrntweather, lat, lon};
     }catch(error){
         errorMsg(error);
         throw error;
     }
-    
 };
 
 // Function to get the weather information 
-const weatherGetter = async (lat,lon, city) => {
+const weatherGetter = async (lat,lon) => {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${Apikey}&units=imperial`);
         if(!response.ok){
             throw new Error('Failed to fetch weather condition');
         }
         const data = await response.json();
+        console.log(data);
         const weather = data.list;
-        return {city,weather};
+        return weather;
     }catch(error){
         errorMsg(error);
         throw error;
@@ -67,25 +68,26 @@ const weatherGetter = async (lat,lon, city) => {
 // Function to use latLon and weather getter together and send the information to the createWeatherCard function
 const latLonWeather = (city) => {
     const cardInfo = [];
-    latLonGetter(city)
-        .then(({ lat, lon}) => {
-            return weatherGetter(lat, lon, city); 
-        })
-        .then(({ city, weather }) => {
-            console.log(weather);
-            console.log(city)
-            console.log(weather[0]);
-            for (let i = weather.length - 1; i >= 0; i -= 8) {
-                cardInfo.unshift(weather[i])
-                console.log(weather[i].dt_txt);
-            };
-            cardInfo.unshift(weather[0]);
-            console.log(cardInfo);
-            createWeatherCard(city, cardInfo);
-        })
-        .catch(error => {
-            errorMsg(error);
-        });
+    return currentWeatherGetter(city)  
+    .then(({crrntweather,lat, lon }) => {
+        cardInfo.push(crrntweather);
+        return {lat, lon};
+    })
+    .then(({lat, lon}) => {
+        return weatherGetter(lat, lon);
+    })
+    .then((weather) =>{
+        console.log(weather);
+        for (let i = 6; i < weather.length; i += 8) {
+            cardInfo.push(weather[i]);
+            console.log(weather[i].dt_txt);
+        }
+        console.log(cardInfo);
+        today.empty();
+        forecast.empty();
+        createWeatherCard(cardInfo);
+})
+.catch(error => errorMsg(error));
 };
 
 
@@ -113,27 +115,28 @@ const weatherSearcher = (event) => {
     latLonWeather(city);
 };
 
-const createWeatherCard = (city, weather) => {
+const createWeatherCard = (weather) => {
 
     // Loops over the array to get the information in the cards 
     for(let i = 0; i < weather.length; i++){
 
         // Sets the classes that will be applied if it is the first element or not
-    const colClass = i === 0 ? 'col-md-8 col-g-6 col-xl-4': 'col-md-8 col-lg-6 col-xl-2'
-    const cardBodyClass = i === 0 ? 'card-body p-4' : 'card-body p-2'
-    const headerClass = i === 0 ? 'd-flex' : 'd-flex text-center'
-    const dateClass = i === 0 ? '' : 'me-3'
-    const tempCondclass = i === 0 ? 'd-flex flex-column text-center mt-5 mb-4' : 'd-flex flex-column text-center mt-3 mb-2'
-    const imgclass = i === 0 ? 'bigone' : ''
-    const degrees = i === 0 ? 'display-4 mb-0 font-weight-bold' : 'display-6 mb-0 font-weight-bold'
+    const colClass = i === 0 ? 'col-md-8 col-g-6 col-xl-4': 'col-md-8 col-lg-6 col-xl-2';
+    const cardBodyClass = i === 0 ? 'card-body p-4' : 'card-body p-2';
+    const headerClass = i === 0 ? 'd-flex' : 'd-flex text-center';
+    const dateClass = i === 0 ? '' : 'me-3';
+    const tempCondclass = i === 0 ? 'd-flex flex-column text-center mt-5 mb-4' : 'd-flex flex-column text-center mt-3 mb-2';
+    const imgclass = i === 0 ? 'bigone' : '';
+    const degrees = i === 0 ? 'display-4 mb-0 font-weight-bold' : 'display-6 mb-0 font-weight-bold';
+    const dateparser = i === 0 ?  dayjs.unix(weather[i].dt) :  dayjs(weather[i].dt_txt);
 
         // Creates the html elements 
     const col = $('<div>').addClass(colClass); 
     const card = $('<div>').addClass('card'); 
     const cardBody = $('<div>').addClass(cardBodyClass); 
     const header = $('<div>').addClass(headerClass); 
-    const cityname = $('<h6>').addClass('flex-grow-1').text(city); 
-    const date = $('<h6>').text(dayjs(weather[i].dt_txt).format('M/D/YY')).addClass(dateClass); 
+    const cityname = $('<h6>').addClass('flex-grow-1').text(weather[0].name); 
+    const date = $('<h6>').text( dateparser.format('M/D/YY')).addClass(dateClass); 
     const tempcond = $('<div>').addClass(tempCondclass); 
     const temp = $('<h6>').addClass(degrees).text(`${weather[i].main.temp} ºF`);
     const cond = $('<span>').addClass('small').text(weather[i].weather[0].main);
@@ -176,13 +179,19 @@ const historybtn = (event) => {
 
 
 
-// When the page is fully loaded adds event listeners and loops over the history to get the history buttuns on the page
+// When the page is fully loaded adds event listeners and loops over the history to get the history buttons on the page
 $(document).ready(function () {
     $('#search').on('click', weatherSearcher);
 
     history.forEach(search => {
         renderHistory(search);
     });
+// Defaults a weather search for New York if the history's empty, if not gets the latest search weather
+    if(history.length === 0){
+        latLonWeather('new york');
+    }else{
+        latLonWeather(history[history.length - 1]);
+    }
 
-    historylist.on('click', historybtn)
+    historylist.on('click', historybtn);
 })
